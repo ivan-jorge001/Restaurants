f3ac9f8dec45461e6836fe4ef8c8341b32d78346
Ivan Jorge <ivan.jorge001@mymdc.net>
2017-05-04 18:01:23 -0400

    search page done

--- routes/rest-route.js
+++ routes/rest-route.js
@@ -15,19 +15,52 @@ restRouter.get('/restaurants', (req, res, next) => {
         });
     });
 });
+restRouter.get('/restaurants/search/:cate',(req,res,next)=>{
+  console.log("it got inside the get");
 
+  const type = req.params.cate;
+  res.render('Restaurants-views/rest-search-views.ejs',{type:type, rest:undefined});
+});
+
+restRouter.post('/restaurants/search/:cate',(req,res,next)=>{
+  console.log("it got inside the post");
+  const searchBy = req.params.cate;
+  console.log(`searchBy ============================= ${searchBy}`);
+  if (!req.body.searchValue) {
+res.render('Restaurants-views/rest-search-views.ejs');
+  }
+    console.log(`searchValue ============================= ${req.body.searchValue}`);
+  const searchTerm = new RegExp(req.body.searchValue,"i");
+  // const filter = { [searchBy]: searchTerm };
+  const filter = {};
+  filter[searchBy] = searchTerm;
+
+  RestModel.find(filter,(err,theRest)=>{
+    if (err) {
+      next(err);
+      return;
+    }
+    console.log(`=================>${theRest}`);
+    res.render('Restaurants-views/rest-search-views.ejs',{
+      rest:theRest,
+      type:searchBy
+    });
+  });
+
+});
 
 
 
 restRouter.get('/restaurants/:id', (req, res, next) => {
     const restId = req.params.id;
-    RestModel.find(restId, (err, theRest) => {
-      if (err) {
-        next(err);
-        return;
-      }
-      res.render('Restaurants-views/Rest-details-views.ejs',
-    rest:theRest);
+    RestModel.findById(restId, (err, theRest) => {
+        if (err) {
+            next(err);
+            return;
+        }
+        res.render('Restaurants-views/rest-details-views.ejs', {
+            rest: theRest
+        });
     });
 });
 module.exports = restRouter;
